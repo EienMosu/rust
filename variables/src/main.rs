41f8fce56f3ad8f444b8dc9261bf8bdd3614bd3a
EienMosu <ozkanselcuk@Ozkan-MacBook-Air.local>
2022-05-20 00:57:44 +0200

    recheck optinal exercises

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,11 +2,10 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUTH: i32 = 2;
 
 fn main() {
+    let _ozkan = 6; //if you wanna write unused variables put "_" to begining!
     let (mut missiles, mut ready) = (8, 2);
     println!("Firing {} of my {} missiles...", ready, missiles);
-
-    missiles = missiles - ready;
-    println!("{} missiles left.", missiles);
+    println!("{} missiles left.", missiles - ready);
 
     missiles = STARTING_MISSILES;
     ready = READY_AMOUTH;
